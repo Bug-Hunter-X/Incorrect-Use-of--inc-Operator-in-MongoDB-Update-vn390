1039f86d6b2db5938c357bead0bce9ae795eeb42
BugHunter-X <askzenai@gmail.com>
2025-02-11 15:38:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,4 @@
+```javascript
+// Incorrect use of $inc operator in MongoDB update
+db.collection('myCollection').updateOne({"_id": ObjectId("6515f56627f5676e5d654321")}, {"$inc": {"count": "10"}});
+```
